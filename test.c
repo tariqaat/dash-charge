9ce370dc872f9e67c365e34bf008182fc268d601
amlan20 <48976718+amlan20@users.noreply.github.com>
2019-03-27 11:54:42 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,25 +1,7 @@
-#include<stdio.h>
-int find_factorial(int);
+#include <stdio.h>
 int main()
 {
-   int num, fact;
-   
-   printf("\nEnter any integer number:");
-   scanf("%d",&num);
- 
-   
-   fact =find_factorial(num);
- 
-   
-   printf("\nfactorial of %d is: %d",num, fact);
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
    return 0;
-}
-int find_factorial(int n)
-{
-    
-   if(n==0)
-      return(1);
- 
-
-   return(n*find_factorial(n-1));
 }
